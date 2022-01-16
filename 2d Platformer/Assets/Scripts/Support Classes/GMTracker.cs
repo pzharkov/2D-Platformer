c002602faaaefe64aea28f4a372f0baceaa276ea
Pavel Zharkov <p.zharkov@hotmail.com>
2022-01-16 13:59:35 -0500

    Added Resources folder under Assets and moved Prefabs folder inside. This is to enable loading resources programmatically.
    Modified GMTracker: changed GameManager prefab to load programmatically when no GameManager class is found in scene.

--- 2d Platformer/Assets/Scripts/Support Classes/GMTracker.cs
+++ 2d Platformer/Assets/Scripts/Support Classes/GMTracker.cs
@@ -6,16 +6,14 @@ public class GMTracker : MonoBehaviour
 {
     // support class to track if there is GameManager present in the scene and create one if not
     // this is to allow testing scenes independently as the final build will have GameManager instantiated in the pre-load scene and persist when changing scenes
-
-    [SerializeField]
-    private GameObject gameManagerPrefab = null;
-
+    
     void Start()
     {
         GameManager _gameManager = FindObjectOfType<GameManager>();
 
         if (_gameManager == null)
         {
+            GameObject gameManagerPrefab = Resources.Load<GameObject>("Prefabs/Game_Manager");
             GameObject _gameManagerObject = Instantiate(gameManagerPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity);
             _gameManager = _gameManagerObject.GetComponent<GameManager>();
             
