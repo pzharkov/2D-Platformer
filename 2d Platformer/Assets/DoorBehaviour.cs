135b6694f3a575c106cb2ba99490d25c8ec0399f
Pavel Zharkov <p.zharkov@hotmail.com>
2022-01-31 21:27:31 -0500

    Added lock disappear animation to the door object for visual readibility:
    In DoorBehaviour added a IEnumerator to increase localScale over set time lockDisappearTime and then decrease it over the same period.

--- 2d Platformer/Assets/DoorBehaviour.cs
+++ 2d Platformer/Assets/DoorBehaviour.cs
@@ -7,9 +7,13 @@ public class DoorBehaviour : MonoBehaviour
     [SerializeField]
     private GameObject colliderObject;
     [SerializeField]
-    private float disappearTime;
+    private float doorDisappearTime;
+    [SerializeField]
+    private float lockDisappearTime;
     [SerializeField]
     private SpriteRenderer sprite;
+    [SerializeField]
+    private GameObject lockSprite;
     public void Triggered(PlayerManager playerManager)
     {
         if (playerManager.PlayerCanOpenDoor())
@@ -24,19 +28,50 @@ public class DoorBehaviour : MonoBehaviour
     private void OpenDoor()
     {
         colliderObject.SetActive(false);
-        StartCoroutine(Disappear());        
+        
+        StartCoroutine(Disappear());
+        StartCoroutine(LockDisappear());
     }
     private IEnumerator Disappear()
     {
-        float remainingTime = disappearTime;
+        float remainingTime = doorDisappearTime;
 
         while (remainingTime > 0)
         {
-            remainingTime -= Time.deltaTime;            
-            sprite.color = new Color(1f, 1f, 1f, 1f * remainingTime / disappearTime);
+            remainingTime -= Time.deltaTime;
+            sprite.color = new Color(1f, 1f, 1f, 1f * remainingTime / doorDisappearTime);
 
             yield return null;
         }
         Destroy(gameObject);
     }
+    private IEnumerator LockDisappear()
+    {
+        // pop-up
+        float elapsedTime = 0f;
+        Vector2 startingScale = new Vector2(lockSprite.transform.localScale.x, lockSprite.transform.localScale.y);
+
+        while (elapsedTime < lockDisappearTime)
+        {
+            elapsedTime += Time.deltaTime;
+            lockSprite.transform.localScale = startingScale * (1 + elapsedTime / lockDisappearTime);
+
+            yield return null;
+        }
+
+        // then fade
+        float remainingTime = lockDisappearTime;
+        startingScale = new Vector2(lockSprite.transform.localScale.x, lockSprite.transform.localScale.y);
+
+        while (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+            lockSprite.transform.localScale = startingScale * (remainingTime / lockDisappearTime);
+
+            yield return null;
+        }
+
+        lockSprite.SetActive(false);
+    }
+
 }
