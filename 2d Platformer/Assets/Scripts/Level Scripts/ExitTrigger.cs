c2cba358137647576d6071cdc7ed101bff6bd32f
Pavel Zharkov <p.zharkov@hotmail.com>
2022-02-02 19:45:17 -0500

    Added particles to Exit object for visual effects. Saved all objects in Test_Scene as prefabs. Created BlankScene to use as the base line for making levels.

--- 2d Platformer/Assets/Scripts/Level Scripts/ExitTrigger.cs
+++ 2d Platformer/Assets/Scripts/Level Scripts/ExitTrigger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ExitTrigger : MonoBehaviour
-{
+{   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
